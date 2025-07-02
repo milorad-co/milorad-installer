00dbbad69cdb64540bdfe54f69fb02a833bb1150
Milorad Company <jjrowlaand@gmail.com>
2025-07-03 01:36:58 +1200

    Add files via upload

--- milorad-installer.sh
+++ milorad-installer.sh
@@ -10,8 +10,6 @@ mv -fv milorad-installer.sh /bin/milorad/milorad-installer.sh
 cd ..
 rm -rfv milorad-installer
 chmod -fv u+x milorad-installer.sh
-function milorad -a subcmd target os
-    mv -fv /bin/milorad/milorad.fish /home/$USER/.config/fish/functions/milorad.fish
-end
+mv -fv /bin/milorad/milorad.fish /home/$USER/.config/fish/functions/milorad.fish
 funcsave milorad
 echo -e "\nThe Milorad application installer has been set up. Run the command 'milorad help' for more details."
